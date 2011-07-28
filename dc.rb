656c082d6b896d00e51d49263018fbb759c1c68a
Mark <markandrusroberts@gmail.com>
2011-07-28 13:51:30 -0500

    working

--- dc.rb
+++ dc.rb
@@ -1,7 +1,7 @@
 #! /usr/local/bin/ruby
 
 require 'rubygems'
-require 'readline'
+#require 'readline'
 require 'nokogiri'
 require 'class/page.rb'
 require 'class/node.rb'
@@ -10,26 +10,25 @@ require 'class/fieldDataBody.rb'
 require 'class/urlAlias.rb'
 require 'class/nodeAccess.rb'
 require 'class/link.rb'
+require 'util/usage.rb'
 require 'util/scrape.rb'
+require 'util/procUrl.rb'
 require 'util/makeMasthead.rb'
+require 'util/transforms.rb'
 
-if (ARGV[0].nil? || ARGV[0].strip! == '') # || (ARGV[1].nil? || ARGV[1].strip! == '')
-	puts "DC 1.0, a University of Chicago Web Express to TemplaTron site converter."
-	# puts "Usage: #{$0} [WE_URL] [OUTPUT_SQL]"
-	puts "Usage: #{$0} [WE_URL]"
-	puts "Example: #{$0} crimelab.uchicago.edu\n\n"
-	puts "Mail bug reports and suggestions to <andrus@uchicago.edu>."
-	exit
-end
-
-# Read in the initial Templatron SQL file
-baseSql = File.read('sql/base.sql')
+# Validate arguments & print usage
+if (ARGV[0].nil? || ARGV[0].strip! == '') then usage(); exit; end
 
 # Download the entire target Web Express site using `wget'
 webExpressUrl = ARGV[0]
 wgetDir = 'tmp/'
+puts "Downloading `" + webExpressUrl + "'..."
+puts "\twget --quiet --recursive --no-clobber --domains #{webExpressUrl} #{webExpressUrl} -P #{wgetDir} -l 2\n\n"
 `wget --quiet --recursive --no-clobber --domains #{webExpressUrl} #{webExpressUrl} -P #{wgetDir} -l 2`
+puts "Creating output directory..."
+puts "\trm -rf #{'out/' + webExpressUrl + '/'}"
 `rm -rf #{'out/' + webExpressUrl + '/'}`
+puts "\tmkdir #{'out/' + webExpressUrl + '/'}\n\n"
 `mkdir #{'out/' + webExpressUrl + '/'}`
 
 # NOTE: Assumes BSD version of `find'!
@@ -39,21 +38,16 @@ found = `find #{sitePath.sub(/\/$/, '')} -name index.html`
 # Build the pool of links according to the target sites navigation lists
 # NOTE: We only need to traverse `index.html' pages
 puts "Building tree structure from the following files..."
-siteTitle = ''
 linkPool = []
 found.each_line do |file|
 	file.strip!
 	puts "\t" + file
 	doc = Nokogiri::HTML.parse(File.read(file))
-	# Record the site title
-	if file == sitePath + 'index.html'
-		siteTitle = doc.css('title')[0].content.split('|')[0]
-	end
 	# Record all top-level navigation entries
 	doc.css('ul.sectionname li a').each do |a|
 		href = a['href'].strip
 		tailStr = File.directory?(sitePath + href) ? 'index.html' : ''
-		link = Link.new(a.content.strip, href, sitePath + href.sub(/^\//, '') + tailStr, [])
+		link = Link.new(a.content.strip, procUrl(href, webExpressUrl), sitePath + procUrl(href, webExpressUrl) + tailStr, [])
 		linkPool.push(link)
 	end
 	linkPool.uniq!
@@ -62,18 +56,19 @@ found.each_line do |file|
 	# Parent link
 	doc.css('ul.sectionhead li a').each do |a|
 		parentLinkText += a.content.strip
+		parentLinkUrl = procUrl(a['href'].strip, webExpressUrl)
 		childLinkPool = []
 		if parentLinkText != ''
 			# Child links
 			doc.css('ul.sectionhead + ul li a').each do |b|
 				href = b['href'].strip
-				link = Link.new(b.content.strip, href, sitePath + href.sub(/^\//, ''), [])
+				link = Link.new(b.content.strip, procUrl(href, webExpressUrl), sitePath + procUrl(href, webExpressUrl), [])
 				childLinkPool.push(link)
 			end
 			childLinkPool.uniq!
 			# Attach child links to parent link
 			for link in linkPool do
-				if link.linkText === parentLinkText
+				if link.linkText === parentLinkText || link.relativePath === parentLinkUrl
 					link.children = childLinkPool
 				end
 			end
@@ -82,52 +77,23 @@ found.each_line do |file|
 	linkPool.uniq!
 end
 
-puts "\n" + "Site Title: " + siteTitle
-puts "Making Masthead Image..."
-makeMasthead(siteTitle, 'out/' + webExpressUrl + '/masthead.gif')
+puts ""
 
-# Build "New" UrlAlias with input from user
-def newUrlAlias(page)
-	puts "Title:    " + page.menuLink.linkText
-	puts "URL:      " + page.urlAlias.linkPath
-	print "New URL:> page/"
-	url = "page/" + Readline.readline
-	puts "\n"
-	page.newUrlAlias = UrlAlias.new(getLastUrlAliasId(), page.id, url.strip)
-end
+# Process Site Title
+print "Site Title: "
+#siteTitle = Readline.readline
+siteTitle = $stdin.gets
+print "Generating Masthead Image... "
+makeMasthead(siteTitle, 'out/' + webExpressUrl + '/masthead.png')
+puts "out/" + webExpressUrl + "/masthead.png\n\n"
 
-# Transform the pool of links into our SQL-generating `Page' objects
-def transformLinkPool(linkPool)
-	linkPool.map do |link|
-		# Process children first
-		children = transformLinkPool(link.children)
-		content = scrape(link.filePath)
-		# Process parent
-		page = Page.new(link.linkText, content, link.relativePath, children)
-		# Prompt for "New" UrlAlias
-		newUrlAlias(page)
-		page
-	end
-end
+# Print notice before request for new URLs
+puts "Transforming `#{webExpressUrl}'...\n\n"
+puts "\tPlease provide new URLs for the converted pages."
+puts "\tNOTE: The original URLs will also work on the TemplaTron site.\n\n"
 
-puts "\nConverting `#{webExpressUrl}'..."
-puts "Please provide new URLs for the converted pages."
-puts "NOTE: The old URLs will also work on the TemplaTron site.\n\n"
+# Transform linkPool, and write SQL to file
+outputSql = to_sql(linkPool)
+File.open('out/' + webExpressUrl + '/db.sql', 'w') { |f| f.write(outputSql) }
+puts "Output saved to `#{'out/' + webExpressUrl + '/'}'."
 
-# Transform and collect SQL
-outputSql = ''
-pages = transformLinkPool(linkPool)
-# puts baseSql
-outputSql += baseSql
-i = 0
-pages.each do |page|
-	page.menuLink.weight = -50 + i
-	i += 1
-	# puts page.to_s
-	outputSql += page.to_s
-end
-
-# Write SQL to file
-File.open('out/' + webExpressUrl + '/db.sql', 'w') {|f| f.write(outputSql)}
-
-puts "\nOutput saved to `#{'out/' + webExpressUrl + '/'}'."
